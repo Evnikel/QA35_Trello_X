84f3ea16e130c301ca9ba6a1f4ed61cb8990a205
Evnikel <evnikel@gmail.com>
2022-10-02 19:29:46 +0300

    adedd class

---
+++ src/test/java/Tests/Login.java
@@ -0,0 +1,47 @@
+package Tests;
+
+import model.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Login extends TestBase {
+    @BeforeTest
+    public void preConditions() {
+        if (app.getUser().isLogged()) {
+            app.getUser().logOut();
+        }
+    }
+
+
+
+    @Test
+    public void loginPositive1() {
+        User user = new User().withEmail("evnikel@gmail.com").withPassword("EB106201!");
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillLoginForm(user);
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+    }
+
+    @Test
+    public void loginPositive2() {
+
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillLoginForm2("evnikel@gmail.com","EB106201eb!");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+
+    }
+
+}
